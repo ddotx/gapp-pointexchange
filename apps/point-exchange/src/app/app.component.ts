ac3e5466895e943ffa2e7eb0e5fd636f11d053dd
DomeMMN <domemacintosh@gmail.com>
2019-11-23 17:31:42 +0700

    initial commit

---
+++ apps/point-exchange/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'point-exchange';
+}
